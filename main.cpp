2e46671e41faf1dfbd3de77980eb406bed332f3a
23002610 <161761998+23002610@users.noreply.github.com>
2024-03-15 20:31:17 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double merchCost, Salary, annualRent, electricityBill, profit;
+
+
+    cout << "Enter the total cost of merchandise: ";
+    cin >> merchCost;
+
+    cout << "Enter the total salary of employees : ";
+    cin >> Salary;
+
+    cout << "Enter the annual rent: ";
+    cin >> annualRent;
+
+    cout << "Enter the estimated electricity cost: ";
+    cin >> electricityBill;
+
+    cout << "Enter the desired net profit percentage: ";
+    cin >> profit;
+
+
+    double expenses = merchCost + Salary + annualRent + electricityBill;
+    double desiredProfitAmount = (profit / 100) * merchCost;
+    double totalRevenueNeeded = expenses + desiredProfitAmount;
+
+
+    double saleDiscount = 0.15;
+    totalRevenueNeeded /= (1 - saleDiscount);
+
+
+    double markupPercentage = (totalRevenueNeeded / merchCost - 1) * 100;
+
+
+    cout << "To achieve a net profit of approximately " << profit << "%, ";
+    cout << "the merchandise should be marked up by approximately " << markupPercentage << "%." << endl;
+
+    return 0;
+}
